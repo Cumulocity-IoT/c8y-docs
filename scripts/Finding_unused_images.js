05e0d2500342cdc93c265899f245abcc8407b2d2
logu1411 <lokesh.guthi@cumulocity.com>
2024-10-30 09:04:59 +0100

    Cleaned up code to make it simpler

--- scripts/Finding_unused_images.js
+++ scripts/Finding_unused_images.js
@@ -4,21 +4,14 @@ const path = require('path');
 const fileExtensionsToCheck = ['.html', '.css', '.js', '.md', '.toml'];
 const imageExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.svg'];
 
-// Function to collect a list of all images in the specified folder and its subfolders
-async function getImagesFromSpecificPath(imageFolder, imageExtensions) {
-    const allFiles = await fs.readdir(imageFolder, { recursive: true });
+// Function to get all files with specific extensions from a folder and its subfolders
+async function getFilesByExtensions(folderPath, extensions) {
+    const allFiles = await fs.readdir(folderPath, { recursive: true });
     return allFiles
-        .map(file => path.join(imageFolder, file))
-        .filter(file => imageExtensions.some(ext => file.toLowerCase().endsWith(ext)));
-}
-
-// Function to collect a list of all files with the specified extensions in the project folder
-async function getFilesByExtension(projectFolder, extensions) {
-    const allFiles = await fs.readdir(projectFolder, { recursive: true });
-    return allFiles
-        .map(file => path.join(projectFolder, file))
+        .map(file => path.join(folderPath, file))
         .filter(file => extensions.some(ext => file.toLowerCase().endsWith(ext)));
 }
+
 
 // Function to find image references in the specified files
 async function findImageFiles(filePaths) {
@@ -44,13 +37,12 @@ async function writeToCsv(unusedImagesPaths, outputFile) {
 
 // To find and save unused images
 async function findUnusedImages(imageFolder, projectFolder, outputCsv, imageExtensions) {
-    const imageFiles = await getImagesFromSpecificPath(imageFolder, imageExtensions);
+    const imageFiles = await getFilesByExtensions(imageFolder, imageExtensions);
     const imageFilesWithPaths = Object.fromEntries(imageFiles.map(image => [path.basename(image), image]));
-    const projectFiles = await getFilesByExtension(projectFolder, fileExtensionsToCheck);
+    const projectFiles = await getFilesByExtensions(projectFolder, fileExtensionsToCheck);
     const referencedImages = await findImageFiles(projectFiles);
     const unusedImages = new Set(Object.keys(imageFilesWithPaths).filter(img => !referencedImages.has(img)));
     const unusedImagesWithPaths = Array.from(unusedImages).map(img => imageFilesWithPaths[img]);
-
     await writeToCsv(unusedImagesWithPaths, outputCsv);
     console.log(`Unused images: ${unusedImages.size}`);
     console.log(`Unused image paths from '${imageFolder}' have been saved to: ${outputCsv}`);
