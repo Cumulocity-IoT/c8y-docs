93ca674054fcfb9e35ab5842813242c924211164
Beate Rixen <beate.rixen@cumulocity.com>
2024-11-20 14:47:47 +0100

    changed spelling of Düsseldorf

--- static/files/edge/backup.sh
+++ static/files/edge/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2024-present Cumulocity GmbH, Düsseldorf, Germany and/or its affiliates and/or their licensors.
+# Copyright (c) 2024-present Cumulocity GmbH, Duesseldorf, Germany and/or its affiliates and/or their licensors.
 # Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Cumulocity GmbH.
 #
 
