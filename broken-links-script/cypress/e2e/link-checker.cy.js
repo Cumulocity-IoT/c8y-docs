6f628f9bcf342ae7291c2422db5bd4ec654fab18
logu1411 <lokesh.guthi@cumulocity.com>
2025-04-29 18:03:12 +0200

    Fix fragment check to validate only existing IDs

--- broken-links-script/cypress/e2e/link-checker.cy.js
+++ broken-links-script/cypress/e2e/link-checker.cy.js
@@ -8,8 +8,7 @@ describe('Link and Routing Validation - Individual URL Checks', () => {
     const escRegExp = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     const escFragment = escRegExp(fragment);
     const regexId = new RegExp(`id=["']${escFragment}["']`);
-    const regexHref = new RegExp(`href=["']#${escFragment}["']`);
-    return regexId.test(htmlContent) || regexHref.test(htmlContent);
+    return regexId.test(htmlContent);
   };
 
   const expectFragmentExists = (htmlContent, fragment) => {
