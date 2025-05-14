3a84662881372f1e00440868542603b42edd44f8
logu1411 <lokesh.guthi@cumulocity.com>
2025-05-14 11:45:57 +0200

    update shortcodes and uncaught exception

--- broken-links-script/cypress/support/e2e.js
+++ broken-links-script/cypress/support/e2e.js
@@ -12,5 +12,48 @@ Cypress.on('uncaught:exception', (err) => {
     if (err.message.includes('jQuery is not defined')) {
       return false;
     }
+    if (err.message.includes('folderIndex is not defined')) {
+      return false;
+    }
+    if (err.message.includes('top.loadFrames is not a function')) {
+      return false;
+    }
+    if (err.message.includes('ResizeObserver loop completed with undelivered notifications.')) {
+      return false;
+    }
+    if (err.message.includes('gd')) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of undefined (reading 'renderer')")) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of null (reading '__k')")) {
+      return false;
+    }
+    if (err.message.includes("Cannot set properties of null (setting 'onfocus')")) {
+      return false;
+    }
+    if (err.message.includes('No ad placements found.')) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of undefined (reading 'TenantFeatures')")) {
+      return false;
+    }
+    if (err.message.includes("jira/featureflags/feature-manager missing jira/util/top-same-origin-window")) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of undefined (reading 'oSort')")) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of undefined (reading 'split')")) {
+      return false;
+    } 
+    if (err.message.includes("Error: socket hang up")) {
+      return false;
+    }
+    if (err.message.includes("Cannot read properties of null (reading 'setAttribute')")) {
+      return false;
+    }
+
   });
   
