18db23eb8f925113943dd2a3a10f51ef9e16f5c8
logu1411 <lokesh.guthi@cumulocity.com>
2026-03-16 11:16:56 +0100

    Update script to fix timeout links

--- broken-links-script/cypress/e2e/link-checker.cy.js
+++ broken-links-script/cypress/e2e/link-checker.cy.js
@@ -185,22 +185,34 @@ describe('Link and Routing Validation - Individual URL Checks', () => {
         cy.visit(url, {failOnStatusCode: false, headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/122.0 Safari/537.36' }});
         checkRegularFragment(fragment);
       }
-      else {
-        cy.request({
-          url: url,
-          failOnStatusCode: false
-        }).then((response) => {
-          const contentType = response.headers['content-type'] || '';
-          if (!contentType.includes('text/html')) {
-            cy.log(`Non-HTML content detected for ${url}, skipping cy.visit()`);
-            expect(response.status).to.be.oneOf([200, 201, 202, 203, 204, 301, 302, 304]);
-            expect(response.body).not.to.be.empty;
-          } else {
-            cy.visit(url);
-            cy.document().its('body').should('not.be.empty');
-          }
-        });
-      }
+ else {
+
+  cy.log(`Validating HTML page with curl: ${url}`);
+
+  cy.task('curlRequest', url).then(({ status, content }) => {
+
+    expect(status)
+      .to.be.oneOf([200,301,302,429]);
+
+    const parser = new DOMParser();
+
+    const doc = parser.parseFromString(
+      content,
+      'text/html'
+    );
+
+    const body = doc && doc.body
+      ? doc.body.textContent.trim()
+      : '';
+
+    expect(
+      body,
+      `<body> content for ${url} should not be empty`
+    ).to.not.be.empty;
+
+  });
+
+}
       
       completedTests++;
     });
